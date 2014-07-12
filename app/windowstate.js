541277affda91727bcf7e6a5ab00b918c73d4208
Sam <splavigne@gmail.com>
2014-07-12 16:40:19 -0400

    start windowstate

---
+++ app/windowstate.js
@@ -0,0 +1,43 @@
+//thinking about how to preserve window state bewteen sessions
+
+//var windows = [
+  //{
+    //projectPath: '/some/path',
+    //currentFilePath: '/some/path/file.js',
+    //width: 100,
+    //height: 100,
+    //x: 10,
+    //y: 10,
+    //sidebarWidth: 10,
+    //debugWidth: 10,
+    //fullscreen: false,
+    //focused: false
+  //}
+//];
+
+//var winState = {};
+
+//var win = gui.Window.get();
+
+//win.on('focus', function() {
+  //winState.focused = true;
+//});
+
+
+//win.on('blur', function() {
+  //winState.focused = false;
+//});
+
+//win.on('move', function(x, y) {
+  //winState.x = x;
+  //winState.y = y;
+//});
+
+//win.on('resize', function(width, height) {
+  //winState.width = width;
+  //winState.height = height;
+//});
+
+//win.on('close', function() {
+  //this.close(true);
+//});
