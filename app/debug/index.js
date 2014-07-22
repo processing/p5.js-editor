06ca83c2d1c557b7429a313532a9a3d8a96827eb
Sam <splavigne@gmail.com>
2014-07-22 17:23:34 -0400

    js for horizonantal/vertical placement of debug console

--- app/debug/index.js
+++ app/debug/index.js
@@ -7,6 +7,26 @@ module.exports = {
   methods: {
 
     startDrag: function(e) {
+      if ($('body').hasClass('horizontal')) {
+        this.horizonatlDrag(e);
+      } else {
+        this.verticalDrag(e);
+      }
+    },
+
+    horizonatlDrag: function(e) {
+      var container = $('#debug-container');
+      var startY = e.clientY;
+      var startHeight = container.height();
+      $(document).on('mousemove', function (e) {
+        container.css({height: startHeight - (e.clientY - startY)});
+        ace.resize();
+      }).on('mouseup', function (e) {
+        $(document).off('mouseup').off('mousemove');
+      });
+    },
+
+    verticalDrag: function(e) {
       var container = $('#debug-container');
       var startX = e.clientX;
       var startWidth = container.width();
