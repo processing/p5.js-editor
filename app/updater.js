813d1adc1b0a5e630e1f9a237d7adbfef3e86a01
Sam <splavigne@gmail.com>
2014-07-30 15:28:27 -0400

    disable cache for checking for new versions

--- app/updater.js
+++ app/updater.js
@@ -6,13 +6,14 @@ var downloadURL = 'https://github.com/antiboredom/jside/releases/download/';
 module.exports.check = function() {
   if (nodeGlobal.checkedUpdate === true) return false;
   nodeGlobal.checkedUpdate = true;
-  $.get(packageURL, function(data) {
-    var latestVersion = JSON.parse(data).version;
+  $.ajax({url: packageURL, success: update, cache: false, dataType: 'json'});
+  function update(data) {
+    var latestVersion = data.version;
     if (latestVersion != manifest.version) {
       var shouldDownload = confirm('A newer version of P5 is available. Do you want to download it?');
       if (shouldDownload) {
-        gui.Shell.openExternal(downloadURL + latestVersion + '/p5.zip');
+        gui.Shell.openExternal(downloadURL + 'v' + latestVersion + '/p5.zip');
       }
     }
-  });
+  }
 };
