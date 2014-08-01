840353b3fe361ef0e1d5e2faa3c08428f311dc19
Sam <splavigne@gmail.com>
2014-08-01 16:20:07 -0400

    use semver module for version checking

--- app/updater.js
+++ app/updater.js
@@ -1,5 +1,6 @@
 var $ = require('jquery');
 var manifest = nodeRequire('./package.json');
+var semver = nodeRequire('semver');
 var packageURL = 'https://raw.githubusercontent.com/antiboredom/jside/master/package.json?v='  + new Date().getTime();
 var downloadURL = 'https://github.com/antiboredom/jside/releases/download/';
 
@@ -8,11 +9,10 @@ module.exports.check = function() {
   nodeGlobal.checkedUpdate = true;
   $.ajax({url: packageURL, success: update, cache: false, dataType: 'json'});
   function update(data) {
-    var latestVersion = data.version;
-    if (latestVersion != manifest.version) {
+    if (semver.gt(data.version, manifest.version)) {
       var shouldDownload = confirm('A newer version of P5 is available. Do you want to download it?');
       if (shouldDownload) {
-        gui.Shell.openExternal(downloadURL + 'v' + latestVersion + '/p5.zip');
+        gui.Shell.openExternal(downloadURL + 'v' + data.version + '/p5.zip');
       }
     }
   }
