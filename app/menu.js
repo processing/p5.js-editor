de978a544bc5976b683f0c302799e157343b2ecf
Jason Alderman <jasonald@gmail.com>
2015-05-27 18:13:30 -0400

    Fixed Open Recent menu, and added a Clear Recent Projects action at bottom of submenu to clear the Open Recent menu.

--- app/menu.js
+++ app/menu.js
@@ -7,6 +7,7 @@ var fileMenu = new gui.Menu();
 var help = new gui.Menu();
 var win = gui.Window.get();
 var recentFilesMenu = new gui.Menu();
+var openRecent;
 
 module.exports.setup = function(app) {
   fileMenu.append(new gui.MenuItem({ label: 'New File', modifiers: 'cmd', key: 'n', click: function(){
@@ -21,7 +22,7 @@ module.exports.setup = function(app) {
     $('#openFile').trigger('click');
   }}));
 
-  var openRecent = new gui.MenuItem({label: 'Open Recent'});
+  openRecent = new gui.MenuItem({label: 'Open Recent'});
   openRecent.submenu = recentFilesMenu;
   fileMenu.append(openRecent);
 
@@ -39,6 +40,7 @@ module.exports.setup = function(app) {
 
   fileMenu.append(new gui.MenuItem({ label: 'Save Project As...', modifiers: 'alt-shift-cmd', key: 's', click: function(){
     $('#saveProject').trigger('click');
+    
   }}));
 
   fileMenu.append(new gui.MenuItem({ type: 'separator' }));
@@ -74,10 +76,24 @@ module.exports.updateRecentFiles = function(app, path) {
 
   localStorage.recentFiles = JSON.stringify(recentFiles);
 
+  // clear the recentFilesMenu because we're going to repopulate it
+  recentFilesMenu = new gui.Menu();
+  openRecent.submenu = recentFilesMenu;
+
+  // iterate through the recentFiles and append to the openRecent submenu
   recentFiles.forEach(function(p) {
     var m = new gui.MenuItem({ label: Path.basename(p), click: function() {
       app.openProject(p);
     }})
     recentFilesMenu.append(m);
   });
+  if (recentFiles.length > 0) {
+    recentFilesMenu.append(new gui.MenuItem({type: 'separator'}));
+    recentFilesMenu.append(new gui.MenuItem({label: 'Clear Recent Projects', click: function(){
+      localStorage.recentFiles = [];
+      module.exports.updateRecentFiles(app);
+    }}));
+  }
+
+  openRecent.enabled = (recentFiles.length !== 0);
 };
