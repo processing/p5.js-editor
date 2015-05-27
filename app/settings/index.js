2483393f4c8834d1e50a68ab841f32ba0d85e56f
Sam <splavigne@gmail.com>
2015-05-27 13:16:41 -0400

    fix undefined tab settings

--- app/settings/index.js
+++ app/settings/index.js
@@ -1,9 +1,23 @@
 module.exports = {
   template: require('./template.html'),
 
+  data: {
+    tabSizeDisplay: 1
+  },
+
   ready: function() {
+    if (parseInt(this.tabSize) < 1) {
+      this.tabSize = 1;
+    }
+    this.tabSizeDisplay = this.tabSize;
   },
 
   methods: {
+    updateTabSize: function(e) {
+      var parsed = parseInt(e.target.value);
+      this.tabSize = parsed >= 1 ? parsed : 1;
+      this.tabSizeDisplay = this.tabSize;
+    }
   }
+
 };
