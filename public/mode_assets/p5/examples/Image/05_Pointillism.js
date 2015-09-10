aa701765dc16bd52dfec62e16929cad1a3d7da00
Sam <splavigne@gmail.com>
2015-09-10 14:48:25 -0400

    update examples

--- public/mode_assets/p5/examples/Image/05_Pointillism.js
+++ public/mode_assets/p5/examples/Image/05_Pointillism.js
@@ -10,14 +10,18 @@
 var img;
 var smallPoint, largePoint;
 
+function preload() {
+  img = loadImage("assets/moonwalk.jpg");
+}
+
 function setup() {
   createCanvas(720, 400);
-  img = loadImage("assets/moonwalk.jpg");
   smallPoint = 4;
   largePoint = 40;
   imageMode(CENTER);
   noStroke();
   background(255);
+  img.loadPixels();
 }
 
 function draw() {
