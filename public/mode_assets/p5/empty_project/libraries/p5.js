eab1ef2021d4401f714b64788b004d9c636c5878
Sam <splavigne@gmail.com>
2015-12-11 12:46:24 -0500

    update p5

--- public/mode_assets/p5/empty_project/libraries/p5.js
+++ public/mode_assets/p5/empty_project/libraries/p5.js
@@ -1,4 +1,4 @@
-/*! p5.js v0.4.18 November 09, 2015 */
+/*! p5.js v0.4.19 November 11, 2015 */
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.p5 = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
 
 },{}],2:[function(_dereq_,module,exports){
@@ -15938,7 +15938,7 @@ p5.prototype.redraw = function () {
   var userSetup = this.setup || window.setup;
   var userDraw = this.draw || window.draw;
   if (typeof userDraw === 'function') {
-    this.resetMatrix();
+    this.push();
     if (typeof userSetup === 'undefined') {
       this.scale(this._pixelDensity, this._pixelDensity);
     }
@@ -15946,6 +15946,7 @@ p5.prototype.redraw = function () {
     this._registeredMethods.pre.forEach(function (f) {
       f.call(self);
     });
+    this.pop();
     userDraw();
     this._registeredMethods.post.forEach(function (f) {
       f.call(self);
